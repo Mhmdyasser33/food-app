27d6389a13db6bb5c282ad4d5ad5aedb86637f82
Mhmdyasser33 <muhmmed.yasserxi@gmail.com>
2026-07-26 01:17:03 +0300

    organize project architecture and add cors

---
+++ config/allowedOptions.js
@@ -0,0 +1,18 @@
+const allowedOrigin = require("./allowedOrigin")
+
+const corsOptions = [
+    {
+        origin : (origin , callback)=>{
+            if(allowedOrigin.includes(origin) || !origin){
+            /*callback(null , true) means : error : null allowed : true*/
+                callback(null , true) 
+            }else{
+                callback(new Error("Not allowed by CORS"))
+            }
+        },
+        credentials : true,
+        optionsSuccessStatus : 200
+    }, 
+]
+
+module.exports = corsOptions
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const dotenv = require("dotenv").config()
 const express = require("express")
+const cors = require("cors")
 
+const PORT = process.env.PORT || 5001
+const corsOptions = require("./config/allowedOptions")
 const app = express()
+app.use(cors(corsOptions))
+app.use(express.json())
 
 app.get("/" , (req , res)=>{
     res.send("<h1>Welcome to our food app</h1>")
 })
 
 
-app.listen(process.env.PORT , ()=>{
-    console.log(`server is running on port ${process.env.PORT}`)
+app.listen(PORT , ()=>{
+    console.log(`server is running on port ${PORT}`)
 })
